e255089861df2af8da185a9688a3b629f12e01f5
oscarruiz86 <oscarruiz86@gmail.com>
2021-06-04 00:16:47 -0500

    ajuste git ignore

--- .history/spoty_20210531224243.js
+++
@@ -1,6 +0,0 @@
-configSpoty={
-    grant_type:'client_credentials',
-    client_id:'bdfbed63b6d24d3db079f2a7c3eb3a37',
-    client_secret:'29b35d35b35a44bcadc96885e2efc1c0',
-    token:'https://accounts.spotify.com/api/token'
-}
